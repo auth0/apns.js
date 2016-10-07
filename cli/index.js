17d36a7f74dc103050eb0153dfc7b8620953b829
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-10-07 17:55:21 -0300

    Clean up public interface. Just 2 objects: APNS & APNSError

--- cli/index.js
+++ cli/index.js
@@ -1,31 +1,27 @@
 "use strict";
 
-require('dotenv').config();
 const fs = require('fs');
-const apns2 = require('../');
-const Topic = apns2.Topic;
-const Store = apns2.Token;
+const APNS = require('../').APNS;
 
 console.log(`Loading key from path ${process.env.APPLE_KEY_PATH}`);
 const key = fs.readFileSync(process.env.APPLE_KEY_PATH);
 console.log(`Loaded key ${key}`);
 
-const store = new Store(process.env.APPLE_TEAM_ID, {id: process.env.APPLE_KID, pem: key});
+const apns = new APNS(process.env.APPLE_TEAM_ID, {id: process.env.APPLE_KID, pem: key});
 const env = process.env.APPLE_ENV || 'development';
-const topic = new Topic(process.env.APPLE_TOPIC, env);
-store.get().then(token => {
-  console.log(`Generated token ${token}`);
-  const notification = {
-    aps: {
-      alert: 'HTTP/2 APNS PUSH!'
-    }
-  };
-  const device = process.argv[2];
-  return topic.send(notification, device, token);
-}).then(response => {
-  console.log(response);
-  process.exit();
-}).catch(error => {
-  console.error(error);
-  process.exit();
-});
+const notification = {
+  aps: {
+    alert: 'HTTP/2 APNS PUSH!'
+  }
+};
+const device = process.argv[2];
+apns
+  .topic(process.env.APPLE_TOPIC, env)
+  .send(notification, device)
+  .then(response => {
+    console.log(response);
+    process.exit();
+  }).catch(error => {
+    console.error(error);
+    process.exit();
+  });
--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const Token = require('./token');
 const Topic = require('./topic');
 const APNSError = require('./error');
 
+class APNS {
+  constructor(teamId, key) {
+    this.store = new Token(teamId, key);
+  }
+
+  topic(name, environment = 'production') {
+    return {
+      send: (notification, device) => {
+        const topic = new Topic(name, environment);
+        return this.store.get()
+          .then(token => topic.send(notification, device, token));
+      }
+    };
+  }
+}
+
 module.exports = {
-  Token,
-  Topic,
+  APNS,
   APNSError
 };
